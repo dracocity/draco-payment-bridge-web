8ac01ca2facfb5afa41db44439b1f60752b6d237
chakannom <genteelson@gmail.com>
2026-02-12 16:49:25 +0900

    1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,7 +70,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/draco-payment-social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
